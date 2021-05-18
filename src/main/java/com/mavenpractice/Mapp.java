67338b4e56f571c8f56df2859db1d155a393569c
unknown <naveentruly86@gmail.com>
2021-05-18 11:21:53 +0530

    Refractor: logging updated

--- src/main/java/com/mavenpractice/Mapp.java
+++ src/main/java/com/mavenpractice/Mapp.java
@@ -1,20 +1,22 @@
 package com.mavenpractice;
 
+import java.io.IOException;
+
 import org.apache.log4j.Appender;
-import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.FileAppender;
 import org.apache.log4j.Layout;
 import org.apache.log4j.Logger;
-import org.apache.log4j.SimpleLayout;
+import org.apache.log4j.PatternLayout;
 
 public class Mapp 
 {
 	static Logger logger = Logger.getLogger(Mapp.class);
 	
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
 		
-		Layout layout = new SimpleLayout();
-		Appender appender = new ConsoleAppender(layout);
+		Layout layout = new PatternLayout("%d %p %C %M %m %n");
+		Appender appender = new FileAppender(layout,"Mapplog.log");
 		logger.addAppender(appender);
 		logger.debug("debug priority");
 		logger.info("info priority");
